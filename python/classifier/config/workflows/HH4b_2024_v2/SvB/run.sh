9404163790ea871f9ab2f12208f64a3ae07339a9
ChuyuanLiu <chuyuanliu96@gmail.com>
2025-05-09 11:48:50 -0400

    add configs for HH4b_2024_v2 training

---
+++ python/classifier/config/workflows/HH4b_2024_v2/SvB/run.sh
@@ -0,0 +1,42 @@
+export LPCUSER="chuyuanl"
+export CERNUSER="c/chuyuan"
+export WFS="classifier/config/workflows/HH4b_2024_v2/SvB"
+export BASE="root://cmseos.fnal.gov//store/user/${LPCUSER}/HH4b"
+export MODEL="${BASE}/classifier/HH4b_2024_v2/SvB/"
+export WEB="root://eosuser.cern.ch//eos/user/${CERNUSER}/www/HH4b/classifier/HH4b_2024_v2/SvB/"
+export GMAIL=~/gmail.yml
+
+# check port
+if [ -z "$1" ]; then
+    port=10200
+else
+    port=$1
+fi
+
+
+# train and make plots
+./pyml.py \
+    template "user: ${LPCUSER}" $WFS/train.yml \
+    -setting Monitor "address: :${port}" -flag debug
+
+./pyml.py analyze \
+    --results ${MODEL}/ggF_baseline/result.json \
+    -analysis HCR.LossROC \
+    -setting IO "output: ${WEB}" \
+    -setting IO "report: ggF_baseline" \
+    -setting Monitor "address: :${port}"
+
+# evaluate
+./pyml.py \
+    template "user: ${LPCUSER}" $WFS/evaluate.yml \
+    -setting Monitor "address: :${port}"
+
+if [ -e "$GMAIL" ]; then
+    ./pyml.py analyze \
+        -analysis notify.Gmail \
+        --title "SvB jobs done" \
+        --body "All jobs done at $(date)" \
+        --labels Classifier HH4b \
+        -from $GMAIL \
+        -setting Monitor "address: :${port}"
+fi
