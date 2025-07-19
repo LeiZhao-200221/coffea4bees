76885a908dab0947b2ff5d8269d4e1bd84fa36f1
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-07-19 14:17:58 -0400

    Remove unused plotting and physics object files to clean up the codebase

--- python/base_class/php_plots/pb_pdf_to_png.py
+++
@@ -1,7 +0,0 @@
-def pdf_to_png(
-def _has_pdftocairo() -> bool:
-def convert_pdf(src: str, dst: str) -> None:
-def convert_pdf_mp(arg: Tuple[str, str]) -> None:
-def convert_pdfs(paths: Sequence[Tuple[str, str]], n_cores: int = 1) -> None:
-def main() -> None:
-
