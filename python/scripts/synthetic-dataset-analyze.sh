d9fa8a64137be65bfc37a31eed41a736a39a5d40
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-08-26 12:29:00 -0400

    Fix script paths for merging YAML datasets in multiple analysis scripts

--- python/scripts/synthetic-dataset-analyze.sh
+++ python/scripts/synthetic-dataset-analyze.sh
@@ -22,7 +22,7 @@ fi
 cat $OUTPUT_DIR/datasets_synthetic_test.yml
 # echo "############### Modifying dataset file with skimmer ci output"
 # cat python/metadata/datasets_ci.yml
-# python python/src/scripts/merge_yaml_datasets.py -m python/metadata/datasets_HH4b.yml -f python/skimmer/metadata/picoaod_datasets_declustered_data_test_UL18A.yml  -o python/metadata/datasets_synthetic_seed17_test.yml
+# python src/scripts/merge_yaml_datasets.py -m python/metadata/datasets_HH4b.yml -f python/skimmer/metadata/picoaod_datasets_declustered_data_test_UL18A.yml  -o python/metadata/datasets_synthetic_seed17_test.yml
 
 #/builds/johnda/coffea4bees/python/skimmer/GluGluToHHTo4B_cHHH1_UL18/picoAOD_seed5.root
 #/builds/johnda/coffea4bees/python
@@ -38,8 +38,8 @@ cat $OUTPUT_DIR/datasets_synthetic_test.yml
 
 
 echo "############### Running test processor "
-# python python/src/scripts/merge_yaml_datasets.py -m python/metadata/datasets_HH4b.yml -f python/skimmer/metadata/picoaod_datasets_declustered_test_UL18.yml  -o python/metadata/datasets_synthetic_test.yml
-# python python/src/scripts/merge_yaml_datasets.py -m python/metadata/datasets_synthetic_seed17.yml -f python/skimmer/metadata/picoaod_datasets_declustered_GluGluToHHTo4B_cHHH1_Run2_seed17.yml -o python/metadata/datasets_synthetic_seed17.yml
+# python src/scripts/merge_yaml_datasets.py -m python/metadata/datasets_HH4b.yml -f python/skimmer/metadata/picoaod_datasets_declustered_test_UL18.yml  -o python/metadata/datasets_synthetic_test.yml
+# python src/scripts/merge_yaml_datasets.py -m python/metadata/datasets_synthetic_seed17.yml -f python/skimmer/metadata/picoaod_datasets_declustered_GluGluToHHTo4B_cHHH1_Run2_seed17.yml -o python/metadata/datasets_synthetic_seed17.yml
 #cat python/metadata/datasets_synthetic_test.yml
 
 time python runner.py -o test_synthetic_datasets.coffea -d synthetic_data synthetic_mc_GluGluToHHTo4B_cHHH1 -p python/analysis/processors/processor_HH4b.py -y UL18  -op $OUTPUT_DIR/ -c python/analysis/metadata/HH4b_synthetic_data.yml -m $OUTPUT_DIR/datasets_synthetic_test.yml
