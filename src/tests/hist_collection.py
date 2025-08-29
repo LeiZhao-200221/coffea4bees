a6705b227d21a1079b99d9adf6fb4ce7d3a96d0a
ChuyuanLiu <chuyuanliu96@gmail.com>
2025-08-29 14:01:11 -0400

    fix  hist test

--- src/tests/hist_collection.py
+++ src/tests/hist_collection.py
@@ -18,7 +18,8 @@ def nested_hists():
             "flat": [1, 2],
             "nested1": [[10, 20], [30]],
             "nested2": [[[100, 200, 300], [400]], [[500, 600]]],
-        }
+        },
+        depth_limit=1,
     )
     fill(data, weight=1.0)
     return collection.to_dict(nonempty=True)
