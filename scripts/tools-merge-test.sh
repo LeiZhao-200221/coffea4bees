50ea3403d991beb7cec5a0af34306393d626b4ad
Alejandro Gomez Espinosa <alejandro.gomez@cern.ch>
2025-09-08 14:43:41 -0400

    Update output base argument handling in analysis and tools scripts for consistency

--- scripts/tools-merge-test.sh
+++ scripts/tools-merge-test.sh
@@ -2,15 +2,23 @@
 # Source common functions
 source "src/scripts/common.sh"
 
-
-OUTPUT_DIR="${1:-"output"}/analysis_merge_test_job"
-echo "############### Checking and creating output directory"
-if [ ! -d $OUTPUT_DIR ]; then
-    mkdir -p $OUTPUT_DIR
+# Parse output base argument
+OUTPUT_BASE_DIR=$(parse_output_base_arg "output" "$@")
+if [ $? -ne 0 ]; then
+    echo "Error parsing output base argument. Use --output-base DIR to specify the output directory. Default DIR=output/"
+    exit 1
 fi
 
+# Create output directory
+JOB="tools_merge_test"
+OUTPUT_DIR=$OUTPUT_BASE_DIR/$JOB
+create_output_directory "$OUTPUT_DIR"
+
 echo "############### Merging coffea files"
-python coffea4bees/analysis/tools/merge_coffea_files.py -f ${1:-"output"}/analysis_test_job/test_databkgs.coffea ${1:-"output"}/analysis_signals_test_job/test_signal.coffea  -o $OUTPUT_DIR/test.coffea
+python coffea4bees/analysis/tools/merge_coffea_files.py \
+    -f $OUTPUT_BASE_DIR/analysis_test_job/test_databkgs.coffea \
+        $OUTPUT_BASE_DIR/analysis_signals_test_job/test_signal.coffea  \
+    -o $OUTPUT_DIR/test.coffea
 
 ls $OUTPUT_DIR
 
